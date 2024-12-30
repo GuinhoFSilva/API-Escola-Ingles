8cb72d7c2cc7e0e29ef1f0948bf6cd47e341eb25
GuinhoFSilva <guilherme.silva@sptech.school>
2024-12-30 13:33:51 -0300

    realizando métodos CRUD

--- API/controllers/PessoasController.js
+++ API/controllers/PessoasController.js
@@ -1,10 +1,49 @@
 const database = require('../models');
 
 class PessoaController {
+    static async criarPessoa(req, res){
+        const dadosPessoa = req.body
+        try{
+            const create = await database.Pessoas.create(dadosPessoa)
+            return res.status(201).json(create)
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    }
+
     static async selectTabelaPessoas(req, res){
         try {
             const select = await database.Pessoas.findAll()
             return res.status(200).json(select)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+    static async selectPessoa(req, res){
+        const {id} = req.params
+        try {
+            const select = await database.Pessoas.findOne( { where: {id: Number(id)} })
+            return res.status(200).json(select)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+    static async atualizarPessoa(req, res){
+        const {id} = req.params
+        const infosAtualizadas = req.body
+        try {
+            await database.Pessoas.update(infosAtualizadas, {where: { id: Number(id) }})
+            const update = await database.Pessoas.findOne( {where: {id: Number(id)}})
+            return res.status(200).json(update)
+        } catch (error){
+            return res.status(500).json(error.message)
+        }
+    }
+    static async deletarPessoa(req, res){
+        const {id} = req.params
+        try { 
+            await database.Pessoas.destroy( { where: {id: Number(id)} })
+            return res.status(200).json( {mensagem: "Usuário Deletado com Sucesso!"} )
         } catch (error) {
             return res.status(500).json(error.message)
         }
--- API/routes/pessoasRoute.js
+++ API/routes/pessoasRoute.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
-const PessoaController = require('../controllers/PessoasController')
+const PessoaController = require('../controllers/pessoasController')
 
 const router = Router()
 
+router.post('/pessoas', PessoaController.criarPessoa)
 router.get('/pessoas', PessoaController.selectTabelaPessoas)
+router.get('/pessoas/:id', PessoaController.selectPessoa)
+router.put('/pessoas/:id', PessoaController.atualizarPessoa)
+router.delete('/pessoas/:id', PessoaController.deletarPessoa)
 
 module.exports = router
